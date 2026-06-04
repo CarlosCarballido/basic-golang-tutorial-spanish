431cb9d7cce6f09c548399b4566b4f0f96c779c2
Carlos Carballido <carlos.carballido@alumnos.uvigo.es>
2026-06-04 12:43:48 +0200

    Format code in panic_recover.go and ticker.go for consistency

--- codigo/concurrencia/panic_recover.go
+++ codigo/concurrencia/panic_recover.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func dividir(a, b int) int {
-    if b == 0 {
-        panic("division por cero")
-    }
-    return a / b
+	if b == 0 {
+		panic("division por cero")
+	}
+	return a / b
 }
 
 func main() {
-    defer func() {
-        if r := recover(); r != nil {
-            fmt.Println("recuperado de:", r)
-        }
-    }()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recuperado de:", r)
+		}
+	}()
 
-    fmt.Println(dividir(10, 0))
-    fmt.Println("fin normal")
+	fmt.Println(dividir(10, 0))
+	fmt.Println("fin normal")
 }
--- codigo/concurrencia/ticker.go
+++ codigo/concurrencia/ticker.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-    ticker := time.NewTicker(1 * time.Second)
-    defer ticker.Stop()
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
 
-    for i := 1; i <= 3; i++ {
-        <-ticker.C
-        fmt.Println("pulso", i)
-    }
+	for i := 1; i <= 3; i++ {
+		<-ticker.C
+		fmt.Println("pulso", i)
+	}
 }
